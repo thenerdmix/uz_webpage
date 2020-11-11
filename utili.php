8a01c7b5fbe8eabff86a045221a900df1221f5ff
fradeb <francescodebortoli2000@gmail.com>
2020-11-11 23:07:29 +0100

    creata la pagina utili.php

---
+++ utili.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title> Link utili </title>
+
+</head>
+
+<body>
+
+<div class="menu">
+<?php include 'sidebar.php';?>
+</div>
+
+<h1>Qualche link utile</h1>
+<p>In realtà questa lista di link utili mi è comoda come enorme bookmark. </p>
+</body>
+</html>
+
+
+
