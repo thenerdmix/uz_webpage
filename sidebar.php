928a95c9c3ec2bd10171a75075331dd9e462028b
fradeb <francescodebortoli2000@gmail.com>
2020-11-14 00:49:40 +0100

    aggiunti link utili alla sidebar

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,7 @@ echo '
 <a href="/~fradeb/normale.php">Normale</a> -  
 <a href="/~fradeb/interessanti.php">Cose interessanti</a> -
 <a href="/~fradeb/archivio.php">Archivio</a> -
+<a href="/~fradeb/utili.php">Link utili</a> - 
 <a href="/~fradeb/dokuwiki">Omnipedia</a>
 
 <hr>
