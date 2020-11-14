1ccc823d064e9b3283601ed0487be45df74e7bf2
fradeb <francescodebortoli2000@gmail.com>
2020-11-14 21:54:27 +0100

    typo

--- random.js
+++ random.js
@@ -5,3 +5,4 @@ var myPix = new Array("images/lion.jpg","images/tiger.jpg","images/bear.jpg");
 function choosePic(){
     var randomNum = Math.floor(Math.random() * myPix.length);
     document.getElementById("myPicture").src = myPix[randomNum];
+}
