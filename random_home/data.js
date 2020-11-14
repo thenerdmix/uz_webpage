1c9fe9f23bae2d874cdee88345bf221259a348a9
fradeb <francescodebortoli2000@gmail.com>
2020-11-14 22:36:21 +0100

    pics added

--- random_home/data.js
+++ random_home/data.js
@@ -1,11 +1,15 @@
 data = [
     {
-        "name": "Ashwin",
-        "age" : "20"
+        "src": "random_home/pics/tovel.jpg",
+        "caption" : "Il <a href=\"https://it.wikipedia.org/wiki/Lago_di_Tovel\">lago di Tovel</a> in Val di Non è uno tra i laghi alpini più belli d'Europa."
     },
     {
-        "name": "Abhinandan",
-        "age" : "20"
+        "src": "random_home/pics/levico_autunno.jpg",
+        "caption" : "20"
+    },
+    {
+        "src": "random_home/pics/altissimo.jpg",
+        "caption" : "20"
     }
 ];
 
--- random_home/random.js
+++ random_home/random.js
@@ -3,10 +3,5 @@ window.onload = choosePic;
 function choosePic(){
     var r = Math.floor(Math.random() * data.length);
     document.getElementById("immagine").src = data[r].src;
-    document.getElementById("descrizione").innerHTML = data[r].age;
-    alert(data[0].name);
-    alert(data[0].age);
-    alert(data[1].name);
-    alert(data[1].age);
-    alert(data.length);
+    document.getElementById("descrizione").innerHTML = data[r].caption;
 }
