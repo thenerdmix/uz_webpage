3e80411f28ca69f1fe9367b573562d0acf911a64
fradeb <francescodebortoli2000@gmail.com>
2020-11-14 21:52:02 +0100

    starting testing random images

---
+++ random.js
@@ -0,0 +1,7 @@
+window.onload = choosePic;
+
+var myPix = new Array("images/lion.jpg","images/tiger.jpg","images/bear.jpg");
+
+function choosePic(){
+    var randomNum = Math.floor(Math.random() * myPix.length);
+    document.getElementById("myPicture").src = myPix[randomNum];
---
+++ random.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Random Image</title>
+     <script src="random.js"></script>
+</head>
+<body>
+     <img src="images/spacer.gif" width="305" height="312" id="myPicture" alt="some image">
+</body>
+</html>
